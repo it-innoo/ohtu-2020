767c75ac5e76f6dea3fcc4f429cdbe9a200b79fa
jahlgren <jukka.ahlgren@gmail.com>
2020-04-09 12:34:50 +0300

    Checkstyle for Pankki

--- viikko4/tehtavat1-4/MockitoDemo/src/main/java/ohtu/Pankki.java
+++ viikko4/tehtavat1-4/MockitoDemo/src/main/java/ohtu/Pankki.java
@@ -1,10 +1,12 @@
-
 package ohtu;
 
-
 public class Pankki {
-    public void maksa(String tilinumero, int summa, int viite){        
-        System.out.println("siirretty "+summa+" tililtä "+tilinumero +" kaupan tilille, viite oli "+viite);    
+
+    public void maksa(String tilinumero, int summa, int viite) {
+        System.out.println("siirretty "
+                + summa + " tililtä "
+                + tilinumero + " kaupan tilille, viite oli "
+                + viite);
         // täällä tuhansia riviä koodia
     }
 }
