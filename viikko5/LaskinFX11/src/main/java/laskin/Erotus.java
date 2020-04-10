77fdfc7d747901e32d26b0aacabad1e2086457e2
jahlgren <jukka.ahlgren@gmail.com>
2020-04-10 10:27:01 +0300

    4. Laskin ja komento-oliot

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Erotus.java
@@ -0,0 +1,49 @@
+
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class Erotus extends Komento {
+
+    public Erotus(TextField tuloskentta,
+            TextField syotekentta,
+            Button nollaa,
+            Button undo,
+            Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = getArvo();
+        
+        sovellus.miinus(arvo);
+        int laskunTulos = sovellus.tulos();
+
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+        
+        if (laskunTulos == 0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+        System.out.println("undo pressed");
+    }
+    
+    private int getArvo() {
+        try {
+            return Integer.parseInt(syotekentta.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
---
+++ viikko5/LaskinFX11/src/main/java/laskin/Summa.java
@@ -0,0 +1,48 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+
+    public Summa(TextField tuloskentta,
+            TextField syotekentta,
+            Button nollaa,
+            Button undo,
+            Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    
+    @Override
+    public void suorita() {
+        int arvo = getArvo();
+        
+        sovellus.plus(arvo);
+        int laskunTulos = sovellus.tulos();
+
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+        
+        if (laskunTulos == 0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+        System.out.println("undo pressed");
+    }
+
+    private int getArvo() {
+        try {
+            return Integer.parseInt(syotekentta.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
