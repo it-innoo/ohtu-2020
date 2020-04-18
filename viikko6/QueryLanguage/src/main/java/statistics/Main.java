8cb199f6eb8e7346056e63deb1db41573b14c652
jahlgren <jukka.ahlgren@gmail.com>
2020-04-18 15:20:44 +0300

    2. Kyselykieli NHLStatistics-ohjelmaan, osa 1

---
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,44 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // seuraavassa osoitteessa 27.11.2019 päivitetyt tilastot
+        String url = "https://nhl27112019.herokuapp.com/players.txt";
+        // ajan tasalla olevat tilastot osoitteessa
+        // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+
+        Statistics stats = new Statistics(new PlayerReaderImpl(url));
+
+        Matcher m = new And(new HasAtLeast(5, "goals"),
+                new HasAtLeast(5, "assists"),
+                new PlaysIn("PHI")
+        );
+
+        stats.matches(m).forEach((player) -> {
+            System.out.println(player);
+        });
+
+        System.out.println("\n=====\n");
+        m = new And(
+                new Not(new HasAtLeast(1, "goals")),
+                new PlaysIn("NYR")
+        );
+
+        stats.matches(m).forEach((player) -> {
+            System.out.println(player);
+        });
+
+        System.out.println("\n=====\n");
+        m = new And(
+                new HasFewerThan(1, "goals"),
+                new PlaysIn("NYR")
+        );
+
+        stats.matches(m).forEach((player) -> {
+            System.out.println(player);
+        });
+    }
+}
